170311539946d4d175b11191b634d3ff58758824
Lars Hartmann <lars@chaotika.org>
2008-12-04 17:55:55 +0100

    90% completed

--- lib/lpxpak.h
+++ lib/lpxpak.h
@@ -15,6 +15,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <arpa/inet.h>
 
 #define __LP_XPAK_STOP_OFFSET__         4
 #define __LP_XPAK_OFFSET_OFFSET__       8
@@ -60,22 +61,71 @@ lpgetxpakfile(FILE *file)
      lpxpak_t *xpak;
      char *xpakstr;
      char tmp[5];
-     uint32_t xpakoffset;
-     int i;
-     
+     uint32_t xpakoffset, test;
+     int ret;
+
+     /* seek to the start of the STOP string */
      fseek(file, __LP_XPAK_STOP_OFFSET__*-1, SEEK_END);
+     
+#ifdef Debug
+     printf("position after seek: %ld\n", ftell(file));
+#endif
+     
+     /* read in the STOP string */
      fread(tmp, 1, 4, file);
      tmp[4] = '\0';
 
+#ifdef Debug
+     printf("position after read: %ld\n", ftell(file));
+#endif
+     
+#ifdef Debug
+     puts(tmp);
+#endif
+     
+     /* check if the read in string is "STOP" - otherwise this here would be a
+      * non valid / non xpak file */
      if (strcmp(tmp, "STOP") != 0) {
           errno = EINVAL;
           return NULL;
      }
 
+     /* seek to the start of the xpak_offset value */
      fseek(file, __LP_XPAK_OFFSET_OFFSET__*-1, SEEK_END);
+     /* read the offset */
+     
+#ifdef Debug
+     printf("position after seek: %ld\n", ftell(file));
+#endif
+     
      fread(&xpakoffset, 4, 1, file);
+     
+#ifdef Debug
+     printf("position after read: %ld\n", ftell(file));
+#endif
+
+     /* convert it from network to local byteorder */
      xpakoffset = ntohl(xpakoffset);
-     printf("%i\n", xpakoffset);
+     
+#ifdef Debug
+     printf("%u\n", xpakoffset);
+#endif
+     
+     xpakstr = (char *)malloc(xpakoffset+1);
+     fseek(file, (long)xpakoffset*-1, SEEK_END);
+     
+#ifdef Debug
+     printf("position after seek: %ld\n", ftell(file));
+#endif
+     ret = fread(&test, 1, 4, file);
+     printf("returnwert von fread: %u\n", ret);
+
+#ifdef Debug
+     printf("position after read: %ld\n", ftell(file));
+#endif
+
+     test = ntohl(test);
+     printf("%u\n", test);
 
      xpak = lpgetxpakstr(NULL);
      return xpak;
@@ -88,14 +138,16 @@ lpgetxpakpath(char *path)
      lpxpak_t *xpak;
      FILE *xpakfile;
      
+     /* check if path points to a file */
      if (stat(path, &ststr) == -1) 
           return NULL;
      if (! S_ISREG(ststr.st_mode) ) {
           errno = EINVAL;
      }
 
+     /* open the file read-only*/
      xpakfile = fopen(path, "r");
-     
+
      xpak = lpgetxpakfile(xpakfile);
      return xpak;
 }
