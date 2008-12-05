9f49abcb4f2a92b3b8c2e4e40838d99417a367e0
Lars Hartmann <lars@chaotika.org>
2008-12-05 10:55:55 +0100

    changed the flow of the lib (we now use fds to read in the data

--- lib/lpxpak.h
+++ lib/lpxpak.h
@@ -8,6 +8,7 @@
 
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -61,30 +62,26 @@ lpxpak_parse_data(const void *s, size_t length)
 lpxpak_t *
 lpxpak_parse_fd(int fd)
 {
-     FILE *file;
      lpxpak_t *xpak;
-
-     if ( (file = fdopen(fd, "r")) == NULL)
-          return NULL;
-     
-     xpak = lpxpak_parse_file(file);
-     return xpak;
-}
-
-lpxpak_t *
-lpxpak_parse_file(FILE *file)
-{
-     lpxpak_t *xpak;
+     struct stat xpakstat;
      void *xpakdata;
      void *tmp;
      uint32_t xpakoffset;
 
+     if (fstat(fd, &xpakstat) == -1)
+          return NULL;
+
+     if (! S_ISREG(xpakstat.st_mode) ) {
+          errno = EINVAL;
+          return NULL;
+     }
+
      /* seek to the start of the STOP string */
-     fseek(file, __LP_XPAK_STOP_OFFSET__*-1, SEEK_END);
+     lseek(fd, __LP_XPAK_STOP_OFFSET__*-1, SEEK_END);
 
      /* read in the STOP string */
      tmp = malloc(4);
-     fread(tmp, 1, 4, file);
+     read(fd, tmp, 4);
 
      /* check if the read in string is "STOP" - otherwise this here would be a
       * non valid / non xpak file */
@@ -94,41 +91,37 @@ lpxpak_parse_file(FILE *file)
      }
 
      /* seek to the start of the xpak_offset value */
-     fseek(file, __LP_XPAK_OFFSET_OFFSET__*-1, SEEK_END);
+     lseek(fd, __LP_XPAK_OFFSET_OFFSET__*-1, SEEK_END);
 
      /* read the offset */
-     fread(&xpakoffset, 4, 1, file);
+     read(fd, &xpakoffset, 4);
 
      /* convert it from network to local byteorder */
      xpakoffset = ntohl(xpakoffset);
 
      /* read xpak-blob into xpakstr  */
      xpakdata = malloc(xpakoffset);
-     fseek(file, (long)(xpakoffset+__LP_XPAK_OFFSET_OFFSET__)*-1, SEEK_END);
-     fread(xpakdata, (size_t)xpakoffset, 1, file);
+     lseek(fd, (off_t)(xpakoffset+__LP_XPAK_OFFSET_OFFSET__)*-1, SEEK_END);
+     read(fd, xpakdata, (size_t)xpakoffset);
+     
+     return lpxpak_parse_data(xpakdata, (size_t)xpakoffset);
+}
 
-     xpak = lpxpak_parse_data(xpakdata, (size_t)xpakoffset);
-     return xpak;
+lpxpak_t *
+lpxpak_parse_file(FILE *file)
+{
+     int fd;
+
+     fd = fileno(file);
+     return lpxpak_parse_fd(fd);
 }
 
 lpxpak_t *
 lpxpak_parse_path(char *path)
 {
-     struct stat ststr;
-     lpxpak_t *xpak;
-     FILE *xpakfile;
+     int fd;
 
-     /* check if path points to a file */
-     if (stat(path, &ststr) == -1) 
-          return NULL;
-     if (! S_ISREG(ststr.st_mode) ) {
-          errno = EINVAL;
-     }
+     fd = open(path, O_RDONLY);
 
-     /* open the file read-only*/
-     if ( (xpakfile = fopen(path, "r")) == NULL)
-          return NULL;
-
-     xpak = lpxpak_parse_file(xpakfile);
-     return xpak;
+     return lpxpak_parse_fd(fd);
 }
