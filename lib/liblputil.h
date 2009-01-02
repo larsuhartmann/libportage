c98884e84f1489cfeccdbdd485fca7f3c28e31fc
Lars Hartmann <lars@chaotika.org>
2009-01-02 20:03:59 +0100

    finnished doxygen documentation

--- lib/liblputil.h
+++ lib/liblputil.h
@@ -34,15 +34,114 @@
 
 #include <regex.h>
 
+/**
+ * Get regexp match
+ *
+ * Returns a string with the given match from a array of regmatch_t
+ * structs. The Memory for the returned string is obtained by malloc(3) and
+ * can be freed with free(3).
+ *
+ * If an error occurs, \c NULL is returned and errno is set to indicate the
+ * error.
+ *
+ * \param match a array of regmatch_t objects as used by regexec(3).
+ * \param n the wanted element starting by 0 for the first element.
+ * \param s the string that was matched by regexec(3).
+ *
+ * \return the matched string as a null terminated c string.
+ * 
+ * \sa regex(3)
+ *
+ * \b Errors:
+ *
+ * - \c EINVAL one or both of the given pointers was \c NULL
+ * - This function may also fail and set errno for any of the errors specified
+ *   for the routine  lputil_substr().
+ * - This function may also fail and set errno for any of the errors specified
+ *   for the routine malloc(3)
+ */
 char *
 lputil_get_re_match(const regmatch_t *match, int n, const char *s);
 
-char *
-lputil_substr(const char *s, size_t os, size_t oe);
+/**
+ * Copy substring from a string.
+ *
+ * Copies a substring starting at os and ending at oe out of a string s and
+ * returns it. The memory for the returned String is obtained by malloc(3) and
+ * can be freed with free(3).
+ *
+ * lputil_get_re_match() does not check if the requested match is inside the
+ * borders of the given string.
+ *
+ * If an error occurs, \c NULL is returned and errno is set to indicate the
+ * error.
+ *
+ * \param s the string of which we want the substring
+ * \param os the start of the substring
+ * \param oe the length of the substring
+ *
+ * \return the substring as a null terminated substring.
+ *
+ * \b Errors:
+ * - \c EINVAL s is \c NULL.
+ * - This routine may also fail and set errno for any of the errors specified
+ *    for the routine malloc(3).
+ */
+char * lputil_substr(const char *s, size_t os, size_t oe);
 
+/**
+ * Split a String into tokens.
+ *
+ * Splits a string into tokens with a given delimiter. Delimiter Characters at
+ * the Start or the End of the String are ignored. Memory for the returned
+ * Data is obtained by malloc(3) and can be freed with free(3).
+ *
+ * If an error occurs, \c NULL is returned and errno is set to indicate the
+ * error.
+ *
+ * \param s the string to be parsed.
+ * \param delim a set of characters that delim the tokens in the parsed
+ * string.
+ *
+ * \return a array of c strings which is terminated by the value \c NULL.
+ *
+ * \sa strtok(3)
+ *
+ * \b Errors:
+ * 
+ * - \c EINVAL one or both of the given pointers are \c NULL
+ * - This routine may also fail and set errno for any of the errors specified
+ *    for the routine malloc(3).
+ */
 char **
 lputil_splitstr(const char *s, const char *delim);
 
+/**
+ * duplicates a memory region.
+ *
+ * returns a pointer to a new memory region which is a duplicate of the given
+ * memory region. Memory for the new region is obtained by malloc(3) and can
+ * be freed with free(3).
+ *
+ * lputil_memdup() does not check if the memory region pointed to by s is
+ * really \c len bytes long.
+ *
+ * If an error occurs, \c NULL is returned and errno is set to indicate the
+ * error.
+ *
+ * \param s a pointer to the memory region to be copied.
+ * \param len the length of the memory region to be copied in bytes.
+ *
+ * \return a pointer to a duplicate of the memory region.
+ *
+ * \sa strdup(3)
+ *
+ * \b Errors:
+ *
+ * - \c EINVAL s is \c NULL.
+ * - This routine may also fail and set errno for any of the errors specified
+     for the routine malloc(3)
+ */
 void *
 lputil_memdup(const void *s, size_t len);
 
