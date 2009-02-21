50422ac102f5eba81a977301ebcf065de40efd3a
Lars Hartmann <lars@chaotika.org>
2009-02-21 03:18:19 +0100

    updated tests to work with the new object based api and fixed t/Makefile.am

--- t/02_lpxpak_parse.c
+++ t/02_lpxpak_parse.c
@@ -13,26 +13,40 @@ main(void)
      char *path = "02_lpxpak_parse.tbz2";
      char *srcpath;
      
-     lpxpak_t **xpak1;
-     lpxpak_t **xpak2;
+     lpxpak_t *xpak1;
+     lpxpak_t *xpak2;
      lpxpak_blob_t *blob1;
      lpxpak_blob_t *blob2;
      int fd;
 
-     if ( (srcpath = getenv("srcdir")) != NULL )
-          if ( chdir(srcpath) == -1 )
+     if ( (srcpath = getenv("srcdir")) != NULL ) {
+          if ( chdir(srcpath) == -1 ) {
                return EXIT_FAILURE;
+          }
+     }
+
+     /* create initialize xpak objects */
+     if ( (xpak1 = lpxpak_create()) == NULL )
+          return EXIT_FAILURE;
+
+     if ( (xpak2 = lpxpak_create()) == NULL ) {
+          lpxpak_destroy(xpak1);
+          return EXIT_FAILURE;
+     }
+
+     lpxpak_init(xpak1);
+     lpxpak_init(xpak2);
      
      /* open the file read-only*/
      if ( (fd = open(path, O_RDONLY)) == -1 )
           return EXIT_FAILURE;
      
-     if ( (xpak1 = lpxpak_parse_fd(fd)) == NULL ) {
+     if ( lpxpak_parse_fd(xpak1, fd) == -1 ) {
           close(fd);
           return EXIT_FAILURE;
      }
-     if ( strcmp(xpak1[0]->name, "USE" ) != 0 ||
-         strcmp(xpak1[22]->name, "CFLAGS") != 0 ) {
+     if ( strcmp(xpak1->entries[0].name, "USE" ) != 0 ||
+         strcmp(xpak1->entries[22].name, "CFLAGS") != 0 ) {
           lpxpak_destroy(xpak1);
           return EXIT_FAILURE;
      }
@@ -46,7 +60,11 @@ main(void)
           lpxpak_blob_destroy(blob2);
           return EXIT_FAILURE;
      }
-     xpak2 = lpxpak_parse_data(blob1);
+     if ( lpxpak_parse_data(xpak2, blob1) == -1 ) {
+          lpxpak_destroy(xpak1);
+          lpxpak_blob_destroy(blob2);
+          return EXIT_FAILURE;
+     }
      if ( blob1->len == blob2->len) {
           if ( memcmp(blob1->data, blob2->data, blob1->len) != 0 ) {
                lpxpak_destroy(xpak1);
