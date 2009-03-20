7638df9a9154e7d9b382f5d546e7150c01ada1bf
Lars Hartmann <lars@chaotika.org>
2009-03-20 15:26:32 +0100

    added tests for lputil_splitstr.

---
+++ test/01_lputil_splitstr.c
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2009 Lars Hartmann
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *      
+ *    2. Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <util.h>
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define MAXLEN          1024
+#define INPUTFILE       "01_lputil_splitstr.txt"
+
+int main(void)
+{
+     bool has_failed = false;
+     FILE *file;
+     int len, i;
+     char *srcpath, s[MAXLEN], delim[MAXLEN], entry[MAXLEN], t[MAXLEN];
+     char **split;
+
+     if ( (srcpath = getenv("srcdir")) != NULL )
+          if ( chdir(srcpath) == -1 )
+               return EXIT_FAILURE;
+     if ( (file = fopen(INPUTFILE, "r")) == NULL )
+          return EXIT_FAILURE;
+     
+     while ( fgets(s, MAXLEN, file) != NULL ) {
+          s[strlen(s)-1] = '\0';
+          fgets(delim, MAXLEN, file);
+          delim[strlen(delim)-1] = '\0';
+          fgets(t, MAXLEN, file);
+          t[strlen(t)-1] = '\0';
+          len = atoi(t);
+          fgets(entry, MAXLEN, file);
+          entry[strlen(entry)-1] = '\0';
+
+          if ( (split = lputil_splitstr(s, delim)) == NULL )
+               has_failed = true;
+          else {
+               if (strcmp(split[0], entry) != 0)
+                    has_failed = true;
+               for ( i=0; split[i] != NULL; ++i )
+                    ;
+               if ( i != len )
+                    has_failed = true;
+               lputil_splitstr_destroy(split);
+          }
+     }
+     if (has_failed)
+          return EXIT_FAILURE;
+     return EXIT_SUCCESS;
+}
