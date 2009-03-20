c8597bcedb6d7d61d1407dcfa754e7aaab53f45d
Lars Hartmann <lars@chaotika.org>
2009-03-20 19:09:28 +0100

    added test for lputil_intlen.

---
+++ test/01_lputil_intlen.c
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2009 Lars Hartmann
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *      
+ *    2. Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <util.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define INPUTFILE       "01_lputil_intlen.txt"
+#define MAXLEN          1024
+
+int main(void)
+{
+     char s[MAXLEN];
+
+     /* test if lputil_intlen can handle INT_MIN */
+     sprintf(s, "%d", INT_MIN);
+     if ( lputil_intlen(INT_MIN) != strlen(s))
+          return EXIT_FAILURE;
+     /* test if lputil_intlen can handle INT_MAX */
+     sprintf(s, "%d", INT_MAX);
+     if ( lputil_intlen(INT_MAX) != strlen(s))
+          return EXIT_FAILURE;
+     
+     return EXIT_SUCCESS;
+}
