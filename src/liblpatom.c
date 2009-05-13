larsuhartmann/libportage

commit 5de5a3e68be82a9fc36c758b78aff36b0f4dd6c8

small fix.

src/liblpatom.c
@@ -142,7 +142,7 @@ lpatom_create(void)
 }
 
 extern void
-lpatom_init(lpatom_t *atom)
+lpatom_init(lpatom_t *handle)
 {
      atom->name = NULL;
      atom->cat = NULL;

commit f0f08a65711d0fd899a3569e2dc1499b6cc5fdb6

fixed compile error in lpatom introduced by commit 5de5a3e68be82a9fc36c758b78aff36b0f4dd6c8

src/liblpatom.c
@@ -148,16 +148,16 @@ lpatom_init(lpatom_t *handle)
 /*@sets handle@*//*@ensures isnull handle->name, handle->cat, handle->version,
 handle->fname@*/
 {
-     atom->name = NULL;
-     atom->cat = NULL;
-     atom->version = NULL;
-     atom->fname = NULL;
+     handle->name = NULL;
+     handle->cat = NULL;
+     handle->version = NULL;
+     handle->fname = NULL;
 
      /* FIXME: add error handling */
-     (void)regcomp(&atom->regex.atom, LPATOM_RE, REG_EXTENDED);
-     (void)regcomp(&atom->regex.category, LPATOM_RE_CAT, REG_EXTENDED);
-     (void)regcomp(&atom->regex.name, LPATOM_RE_NAME, REG_EXTENDED);
-     (void)regcomp(&atom->regex.version, LPATOM_RE_VER, REG_EXTENDED);
+     (void)regcomp(&handle->regex.atom, LPATOM_RE, REG_EXTENDED);
+     (void)regcomp(&handle->regex.category, LPATOM_RE_CAT, REG_EXTENDED);
+     (void)regcomp(&handle->regex.name, LPATOM_RE_NAME, REG_EXTENDED);
+     (void)regcomp(&handle->regex.version, LPATOM_RE_VER, REG_EXTENDED);
 
      return;
 }